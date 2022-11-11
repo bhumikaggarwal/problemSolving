ff7020ecc473081c6d262959258401658cc464ff
BhumikaAggarwal <aggarwalbhumika99@gmail.com>
2022-11-11 09:14:17 +0530

    Program for checking a prime number

---
+++ primeNumberOrNot.js
@@ -0,0 +1,12 @@
+const num =83;
+let isPrime = 1 ;
+for (i = 2; i < num; i++) {
+  if (num % i == 0) {
+    isPrime = 0 ;
+  } 
+}
+if(isPrime) {
+    console.log("Number is prime") ;
+} else {
+    console.log("Number is not prime") ;
+}
